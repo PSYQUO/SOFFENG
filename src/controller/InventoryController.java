1dc4c1f4cbf438892229fa9a1acbeb6de732009f
Patrick Sy <patrick.alq.sy@gmail.com>
2017-11-06 22:23:20 +0800

    Add guide code for Nixon

--- src/controller/InventoryController.java
+++ src/controller/InventoryController.java
@@ -1,18 +1,34 @@
 package controller;
 
+import controller.ViewManager.ViewManagerException;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 import java.io.IOException;
 
 public class InventoryController extends Controller
 {
+    @FXML
+    private TableView tableviewInvetory;
+
+    @FXML
+    private TableColumn tableInventory, colNumber;
+
     public InventoryController() throws IOException
     {
         initialize(this, "/view/inventory", true);
     }
 
     @Override
-    public void load()
+    public void load() throws ViewManagerException
     {
-
+        if(checkInitialLoad(getClass().getSimpleName()))
+        {
+            /**
+             * Nix, put code here
+             */
+        }
     }
 
     @Override
