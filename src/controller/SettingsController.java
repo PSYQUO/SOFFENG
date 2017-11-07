56dc86d418a32275e3f0c74ce0f1bb09269921b6
Patrick Sy <patrick.alq.sy@gmail.com>
2017-11-07 22:49:00 +0800

    Implement more settings navigation

--- src/controller/SettingsController.java
+++ src/controller/SettingsController.java
@@ -4,13 +4,18 @@ import controller.ViewManager.ViewManagerException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
 public class SettingsController extends Controller
 {
     @FXML
-    private Button buttonMainSettingsClose, buttonPasswordSettingsClose, buttonBackupSettingsClose;
+    private Button buttonMainSettingsClose, buttonPasswordSettingsClose, buttonBackupSettingsClose,
+                   buttonChangePassword, buttonSetBackup;
+
+    @FXML
+    private AnchorPane anchorpaneMainSettings, anchorpanePasswordSettings, anchorpaneBackupSettings;
 
     public SettingsController() throws IOException
     {
@@ -26,6 +31,30 @@ public class SettingsController extends Controller
             {
                 viewManager.switchViews("MainMenuController");
                 clear();
+            });
+
+            buttonChangePassword.addEventHandler(ActionEvent.ACTION, e ->
+            {
+                anchorpaneMainSettings.setVisible(false);
+                anchorpanePasswordSettings.setVisible(true);
+            });
+
+            buttonPasswordSettingsClose.addEventHandler(ActionEvent.ACTION, e ->
+            {
+                anchorpanePasswordSettings.setVisible(false);
+                anchorpaneMainSettings.setVisible(true);
+            });
+
+            buttonSetBackup.addEventHandler(ActionEvent.ACTION, e ->
+            {
+                anchorpaneMainSettings.setVisible(false);
+                anchorpaneBackupSettings.setVisible(true);
+            });
+
+            buttonBackupSettingsClose.addEventHandler(ActionEvent.ACTION, e ->
+            {
+                anchorpaneBackupSettings.setVisible(false);
+                anchorpaneMainSettings.setVisible(true);
             });
         }
     }
