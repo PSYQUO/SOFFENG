13140fd637a0b4e3031e43175a2f00bb9e28c56a
Patrick Sy <patrick.alq.sy@gmail.com>
2017-11-07 22:41:24 +0800

    Add Close Button functionallity to views

--- src/controller/SettingsController.java
+++ src/controller/SettingsController.java
@@ -1,20 +1,33 @@
 package controller;
 
 import controller.ViewManager.ViewManagerException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class SettingsController extends Controller
 {
+    @FXML
+    private Button buttonMainSettingsClose, buttonPasswordSettingsClose, buttonBackupSettingsClose;
+
     public SettingsController() throws IOException
     {
-        initialize(this, "/view/main-settings");
+        initialize(this, "/view/settings");
     }
 
     @Override
     public void load() throws ViewManagerException
     {
-
+        if(checkInitialLoad(getClass().getSimpleName()))
+        {
+            buttonMainSettingsClose.addEventHandler(ActionEvent.ACTION, e ->
+            {
+                viewManager.switchViews("MainMenuController");
+                clear();
+            });
+        }
     }
 
     @Override
