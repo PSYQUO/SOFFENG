c29fd00b96becd83c8a3abab08eecc1c6a21ce90
Patrick Sy <patrick.alq.sy@gmail.com>
2017-11-08 22:05:01 +0800

    Update settings UI, add files view and controller

---
+++ src/controller/FilesController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import controller.ViewManager.ViewManagerException;
+
+public class FilesController extends Controller
+{
+    @Override
+    public void load() throws ViewManagerException
+    {
+
+    }
+
+    @Override
+    public void clear()
+    {
+
+    }
+}
