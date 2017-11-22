0411b3aa2e6412babbcaaa707cdfa45daf8d006f
Jolene Valenzuela <jolene_valenzuela@dlsu.edu.ph>
2017-11-22 22:30:06 +0800

    Created WarningDialog/WarningType files (not complete)

---
+++ src/view/WarningDialog.java
@@ -0,0 +1,9 @@
+package view;
+
+import javafx.scene.control.Dialog;
+
+public class WarningDialog extends Dialog {
+    public WarningDialog() {
+
+    }
+}
---
+++ src/view/WarningType.java
@@ -0,0 +1,5 @@
+package view;
+
+public enum WarningType {
+    WRONG_INPUT
+}
