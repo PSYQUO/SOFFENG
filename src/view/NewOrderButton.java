a9076a9fa72d2d4b701134cbef7217f4bc44e52b
Patrick Sy <patrick.alq.sy@gmail.com>
2017-11-25 01:56:25 +0800

    fix part 1 - digging your grave

--- src/view/NewOrderButton.java
+++ src/view/NewOrderButton.java
@@ -1,43 +1,65 @@
 package view;
 
 import javafx.scene.control.Button;
+import model.Consumable;
+
 import java.text.DecimalFormat;
 
-public class NewOrderButton extends Button {
+public class NewOrderButton extends Button
+{
 
-    public NewOrderButton() {
+    private static final String STYLESHEET_LOCATION = "/view/new-order-button.css";
+
+
+    public NewOrderButton()
+    {
         setPrice(0.00);
     }
 
-    public NewOrderButton(String name) {
+    public NewOrderButton(String name)
+    {
         setName(name);
         setPrice(0.00);
     }
 
-    public NewOrderButton(String name, Double price) {
+    public NewOrderButton(String name, Double price)
+    {
         setName(name);
         setPrice(price);
     }
 
-    public NewOrderButton(Consumable c) {
+    public NewOrderButton(Consumable c)
+    {
         setConsumable(c);
         setAppearance();
         refreshText();
     }
 
-    public void setConsumable(Consumable c) {
+    public void setConsumable(Consumable c)
+    {
         consumable = c;
     }
 
-    public Consumable getConsumable() {
+    public Consumable getConsumable()
+    {
         return consumable;
     }
 
-    public void refreshText() {
+    public void setAppearance()
+    {
+        this.getStylesheets().add(NewOrderButton.STYLESHEET_LOCATION);
+        this.setHeight(150);
+        this.setWidth(150);
+        if(consumable != null)
+            this.setId(consumable.getCategory().getCategoryName().replace(" ", ""));
+    }
+
+    public void refreshText()
+    {
         String text = "";
-        if (name != null)
+        if(name != null)
             text += name + "\n";
-        if (price != null)
+        if(price != null)
             text += df.format((double) price);
         this.setText(text);
     }
@@ -46,3 +68,75 @@ public class NewOrderButton extends Button {
     private Double price;
     private DecimalFormat df = new DecimalFormat("0.00");
 }
+
+//package view;
+//
+//        import javafx.scene.control.Button;
+//        <<<<<<< HEAD
+//import model.Consumable;
+//
+//public class NewOrderButton extends Button {
+//    // location of the CSS for each button
+//    private static final String STYLESHEET_LOCATION = "/view/new-order-button.css";
+//
+//    public NewOrderButton(Consumable c) {
+//        setConsumable(c);
+//        setAppearance();
+//=======
+//
+//        public class NewOrderButton extends Button {
+//            public NewOrderButton() {
+//                setPrice(0.00);
+//            }
+//
+//            public NewOrderButton(String name) {
+//                setName(name);
+//                setPrice(0.00);
+//            }
+//
+//            public NewOrderButton(String name, Double price) {
+//                setName(name);
+//                setPrice(price);
+//            }
+//
+//            public String getName() {
+//                return name;
+//            }
+//
+//            public void setName(String name) {
+//                this.name = name;
+//>>>>>>> parent of 8a4f44f... V2017.11.22 patch v1 (#54)
+//                refreshText();
+//            }
+//
+//            public void setConsumable(Consumable c) {
+//                consumable = c;
+//            }
+//
+//            public Consumable getConsumable() {
+//                return consumable;
+//            }
+//
+//            public void refreshText() {
+//<<<<<<< HEAD
+//                this.setText(consumable.getCodeName()
+//                        + "\n" + consumable.getPrice());
+//            }
+//
+//            public void setAppearance() {
+//                this.getStylesheets().add(NewOrderButton.STYLESHEET_LOCATION);
+//                this.setHeight(150);
+//                this.setWidth(150);
+//                if (consumable != null)
+//                    this.setId(consumable.getCategory().getCategoryName().replace(" ", ""));
+//            }
+//
+//            private Consumable consumable;
+//=======
+//        this.setText(name + "\n" + price);
+//        }
+//
+//        private String name;
+//        private Double price;
+//>>>>>>> parent of 8a4f44f... V2017.11.22 patch v1 (#54)
+//    }
